8dcc3f6f94af471f97edfaf473ffa257aedd6698
Bidexdablitz <bamideledamilola3@gmail.com>
2023-07-05 16:15:53 +0100

    subscription and websocket setup

--- server/src/schema/resolvers.ts
+++ server/src/schema/resolvers.ts
@@ -1,6 +1,35 @@
+import { PubSub } from "graphql-subscriptions";
+import { Context } from "server";
+
+let users = [{ name: "dami" }, { name: "joseph" }];
+
 const resolvers = {
     Query: {
-        users: () => [{ name: "dami" }, { name: "joseph" }],
+        users: (parent, args, { pubsub }: Context) => {
+            pubsub.publish("USER_LIST_REQUESTED", { users });
+            return users;
+        },
+    },
+    Mutation: {
+        addUser: (parent, { name }, { pubsub }: Context) => {
+            let newUser = { name };
+            users.push(newUser);
+            pubsub.publish("USER_ADDED", { newUser });
+            pubsub.publish("USER_LIST_REQUESTED", { users });
+            return newUser;
+        },
+    },
+    Subscription: {
+        users: {
+            subscribe: (parent, args, { pubsub }: Context) => {
+                return pubsub.asyncIterator("USER_LIST_REQUESTED");
+            },
+        },
+        newUser: {
+            subscribe: (parent, args, { pubsub }: Context) => {
+                return pubsub.asyncIterator("USER_ADDED");
+            },
+        },
     },
 };
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,69 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import fs from "fs";
+import path from "path";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { ApolloServer, ContextFunction } from "@apollo/server";
 import resolvers from "./schema/resolvers.ts";
-import typeDefs from "./schema/typeDefs.ts";
+import { fileURLToPath } from "url";
+import { PubSub } from "graphql-subscriptions";
+import { ExpressContextFunctionArgument, expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { createServer } from "http";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/lib/use/ws";
 
-const server = new ApolloServer({ typeDefs, resolvers });
-
-const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+const { json } = bodyParser;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pubsub = new PubSub();
+const typeDefs = fs.readFileSync(path.join(__dirname, "schema/types.gql"), {
+    encoding: "utf8",
 });
 
-console.log(`🚀 Server ready at: ${url}`);
+export type Context = {
+    pubsub: PubSub;
+};
+
+const app = express();
+const httpServer = createServer(app);
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+const server = new ApolloServer<Context>({
+    schema,
+    plugins: [
+        // Proper shutdown for the HTTP server.
+        ApolloServerPluginDrainHttpServer({ httpServer }),
+
+        // Proper shutdown for the WebSocket server.
+        {
+            async serverWillStart() {
+                return {
+                    async drainServer() {
+                        await serverCleanup.dispose();
+                    },
+                };
+            },
+        },
+    ],
+});
+
+await server.start();
+const contextFunc: ContextFunction<[ExpressContextFunctionArgument], Context> = async ({ req, res }) => ({
+    pubsub,
+});
+
+app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    json(),
+    expressMiddleware<Context>(server, { context: contextFunc })
+);
+
+const wsServer = new WebSocketServer({ server: httpServer, path: "/graphql" });
+
+const serverCleanup = useServer({ schema, context: contextFunc }, wsServer);
+
+await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000/`);
