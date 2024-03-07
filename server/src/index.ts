585758f192a32140bb15d66456d89784df9b2a09
joetechster <bamideledamilola3@gmail.com>
2024-03-07 17:48:17 +0100

    tweak

--- server/src/index.ts
+++ server/src/index.ts
@@ -70,7 +70,5 @@ const serverCleanup = useServer(
   },
   wsServer
 );
-await new Promise<void>((resolve) =>
-  httpServer.listen({ port: 4000 }, resolve)
-);
+await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
 console.log(`🚀 Server ready at http://localhost:4000/graphql`);
