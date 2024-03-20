b4a9d5d3307ec0c9e5c91920d5eb7dba41a3f061
joetechster <bamideledamilola3@gmail.com>
2024-03-20 09:30:14 +0100

    added search to symbol picker

--- coinnalyze/src/hooks/useTheme.ts
+++ coinnalyze/src/hooks/useTheme.ts
@@ -1,8 +1,9 @@
 import {Theme} from '../globals';
 import {useSelector} from 'react-redux';
 import {selectTheme} from '../redux_schema/themeSlice';
+import {useMemo} from 'react';
 
 export default function useTheme<T>(styleDecorator: (theme: Theme) => T) {
   const theme = useSelector(selectTheme);
-  return {style: styleDecorator(theme), theme};
+  return useMemo(() => ({style: styleDecorator(theme), theme}), [theme]);
 }
--- coinnalyze/src/screens/SymbolPicker.tsx
+++ coinnalyze/src/screens/SymbolPicker.tsx
@@ -1,30 +1,74 @@
-import {FlatList, ListRenderItem, Pressable, StyleSheet} from 'react-native';
+import {
+  FlatList,
+  ListRenderItem,
+  Pressable,
+  StyleSheet,
+  TextInput,
+  View,
+} from 'react-native';
 import {useAppSelector} from '../redux_schema/hooks';
 import {selectSymbols} from '../redux_schema/symbolsSlice';
-import {memo, useCallback} from 'react';
+import {memo, useCallback, useMemo, useState, useTransition} from 'react';
 import {TickerOfficial} from '../__generated__/graphql';
 import Text from '../components/Text';
-import {Theme, screenPadding, surface} from '../globals';
+import {
+  Theme,
+  fontFamilies,
+  onSurface,
+  screenPadding,
+  surface,
+} from '../globals';
 import useTheme from '../hooks/useTheme';
 import {useOnMounted} from '../hooks/useOnMounted';
 import Loading from '../components/Loading';
 import {StackScreenProps} from '@react-navigation/stack';
 import {StackParamList} from '../App';
 import Symbol from '../components/Symbol';
+import SearchIcon from '../../assets/icons/search-icon.svg';
+import {SearchSource} from 'jest';
 
 export default function SymbolPicker({
   route,
 }: StackScreenProps<StackParamList, 'Pick Symbol'>) {
   const {callback} = route.params;
-  const symbols = useAppSelector(selectSymbols);
+  const {style, theme} = useTheme(styleDecorator);
+  const tickers = useAppSelector(selectSymbols);
+  const [search, onChangeSearch] = useState('');
+  const filteredTickers = useMemo(
+    () =>
+      tickers.filter(t =>
+        t.symbol
+          ?.toLocaleLowerCase()
+          .includes(search.toLocaleLowerCase().trim()),
+      ),
+    [search],
+  );
+
   const renderItem: ListRenderItem<TickerOfficial> = useCallback(({item}) => {
     return <Item ticker={item} onPress={callback} />;
   }, []);
+
   const {mounted} = useOnMounted();
   if (!mounted) return <Loading />;
 
   return (
-    <FlatList data={symbols} renderItem={renderItem} initialNumToRender={20} />
+    <FlatList
+      ListHeaderComponent={
+        <View style={style.inputContainer}>
+          <SearchIcon fill={onSurface(theme)} />
+          <TextInput
+            style={style.input}
+            onChangeText={onChangeSearch}
+            value={search}
+            placeholder="Search"
+          />
+        </View>
+      }
+      contentContainerStyle={style.container}
+      data={filteredTickers}
+      renderItem={renderItem}
+      initialNumToRender={20}
+    />
   );
 }
 
@@ -45,14 +89,30 @@ const Item = memo(({ticker, onPress}: ItemType) => {
 
 function styleDecorator(theme: Theme) {
   return StyleSheet.create({
+    container: {
+      marginHorizontal: screenPadding.paddingHorizontal,
+      gap: 10,
+    },
     item: {
       padding: 10,
-      marginBottom: 10,
       backgroundColor: surface(theme),
-      marginHorizontal: screenPadding.paddingHorizontal,
       borderRadius: 5,
       flexDirection: 'row',
       justifyContent: 'space-between',
+    },
+    input: {
+      color: onSurface(theme),
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      fontFamily: fontFamilies.regular,
+      flex: 1,
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      backgroundColor: surface(theme),
+      alignItems: 'center',
+      paddingLeft: 10,
+      borderRadius: 5,
     },
   });
 }
